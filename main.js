30a3329fd059e6bbd9aabde6ff771f302a52921a
estherpinos <estherpinos1@gmail.com>
2023-09-25 23:33:24 +0200

    Edit [js, add info, adn bonus italian date]

--- main.js
+++ main.js
@@ -55,3 +55,48 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+for (let i = 0; i < posts.length; i++) {
+    let element = posts[i];
+    
+    var data = new Date(element.created).toLocaleDateString('pt-BR').replaceAll('/', '-');
+
+    document.getElementById("container").innerHTML+=`
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${element.author.image}" alt="">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">${data}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${element.content}</div>
+            <div class="post__image">
+                <img src="${element.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>`;
+    
+}
+
+document.getElementsByClassName('like-button').addEventListener('click', function(){
+
+    
+});
+
